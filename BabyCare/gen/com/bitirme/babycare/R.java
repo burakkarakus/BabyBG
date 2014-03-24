515de45ee19b8855c6851c8e44cc3a82028282da
Gokay <Gokayerim@gmail.com>
2014-03-25 01:30:01 +0200

    Gereksiz Commentler ve dosyalar silindi. Server.java ve Client.java commentlenerek bırakıldı gelecek için
    Not birden fazla satırı commentleyeceksen oraları seçip CRTL+Shift+C yap

--- BabyCare/gen/com/bitirme/babycare/R.java
+++ BabyCare/gen/com/bitirme/babycare/R.java
@@ -28,40 +28,27 @@ public final class R {
         public static final int icon_rehber=0x7f020001;
     }
     public static final class id {
-        public static final int OTP_connection=0x7f090003;
-        public static final int action_settings=0x7f090017;
-        public static final int btnBebekTekBaslat=0x7f090009;
-        public static final int btn_Sec=0x7f090013;
-        public static final int button1=0x7f090004;
-        public static final int button_baby=0x7f09000d;
-        public static final int button_make_connection=0x7f09000f;
-        public static final int button_parrent=0x7f09000e;
-        public static final int editText_sendClient=0x7f090014;
-        public static final int icon_rehber=0x7f09000c;
-        public static final int labelTelNo=0x7f090007;
-        public static final int mainText=0x7f090001;
-        public static final int progressBar1=0x7f090002;
-        public static final int progress_wait_connection=0x7f090000;
-        public static final int radioArama=0x7f09000b;
-        public static final int radioBaglantiTuru=0x7f090010;
-        public static final int radioCift=0x7f090011;
-        public static final int radioSMS=0x7f09000a;
-        public static final int radioTek=0x7f090012;
-        public static final int radioUyariTuruGrup=0x7f090008;
-        public static final int send=0x7f090015;
-        public static final int serverTextView=0x7f090016;
-        public static final int textView2=0x7f090005;
-        public static final int txtNumara=0x7f090006;
+        public static final int OTP_connection=0x7f090001;
+        public static final int action_settings=0x7f09000f;
+        public static final int btnBebekTekBaslat=0x7f090007;
+        public static final int btn_Sec=0x7f09000e;
+        public static final int button1=0x7f090002;
+        public static final int icon_rehber=0x7f09000a;
+        public static final int labelTelNo=0x7f090005;
+        public static final int progressBar1=0x7f090000;
+        public static final int radioArama=0x7f090009;
+        public static final int radioBaglantiTuru=0x7f09000b;
+        public static final int radioCift=0x7f09000c;
+        public static final int radioSMS=0x7f090008;
+        public static final int radioTek=0x7f09000d;
+        public static final int radioUyariTuruGrup=0x7f090006;
+        public static final int textView2=0x7f090003;
+        public static final int txtNumara=0x7f090004;
     }
     public static final class layout {
-        public static final int activity_baby_wait_connection=0x7f030000;
-        public static final int activity_bebek_cift=0x7f030001;
-        public static final int activity_bebek_tek=0x7f030002;
-        public static final int activity_main=0x7f030003;
-        public static final int activity_parrent_make_connection=0x7f030004;
-        public static final int anasayfa=0x7f030005;
-        public static final int client=0x7f030006;
-        public static final int server=0x7f030007;
+        public static final int activity_bebek_cift=0x7f030000;
+        public static final int activity_bebek_tek=0x7f030001;
+        public static final int anasayfa=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- BabyCare/src/com/bitirme/babycare/client.java
+++ BabyCare/src/com/bitirme/babycare/client.java
@@ -1,98 +1,97 @@
-package com.bitirme.babycare;
-
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-
-public class client extends Activity {
-
-	private Socket socket;
-
-	private static final int SERVERPORT = 5000;
-	private static String SERVER_IP1 = "10.0.2.";
-	private static int SERVER_IP2;
-	private static String SERVER_IP;
-	
-	
-
-	
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.client);		
-
-		new Thread(new ClientThread()).start();
-	}
-
-	public void onClick(View view) {
-		try {
-			EditText et = (EditText) findViewById(R.id.editText_sendClient);
-			String str = et.getText().toString();
-			PrintWriter out = new PrintWriter(new BufferedWriter(
-					new OutputStreamWriter(socket.getOutputStream())),
-					true);
-			out.println(str);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	class ClientThread implements Runnable {
-
-		@Override
-		public void run() {
-		//TODO EMÝN DEÐÝLÝM
-			
-			try {
-				InetAddress serverAddr = null;
-				while(!serverAddr.isReachable(10000)){ // bu kýsýmdan tam emin deðilim
-				for(int i=1;i<=255;i++){ 
-					
-				SERVER_IP2=i;
-				SERVER_IP=SERVER_IP1+String.valueOf(SERVER_IP2);
-				InetAddress.getByName(SERVER_IP);
-					
-				if(serverAddr.isReachable(10000)){
-					
-					Toast.makeText(getApplicationContext(),
-							"Connected to "+serverAddr,
-							Toast.LENGTH_SHORT);
-					
-					
-				}
-				else{
-					Toast.makeText(getApplicationContext(),
-							"Failed to connect "+serverAddr,
-							Toast.LENGTH_LONG);
-				}
-					
-				}
-				}
-			
-				socket = new Socket(serverAddr, SERVERPORT);
-
-			} catch (UnknownHostException e1) {
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-
-		}
-
-	}
-}
+//package com.bitirme.babycare;
+//
+//import java.io.BufferedWriter;
+//import java.io.IOException;
+//import java.io.OutputStreamWriter;
+//import java.io.PrintWriter;
+//import java.net.InetAddress;
+//import java.net.Socket;
+//import java.net.UnknownHostException;
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.EditText;
+//import android.widget.Toast;
+//
+//public class client extends Activity {
+//
+//	private Socket socket;
+//
+//	private static final int SERVERPORT = 5000;
+//	private static String SERVER_IP1 = "10.0.2.";
+//	private static int SERVER_IP2;
+//	private static String SERVER_IP;
+//	
+//	
+//
+//	
+//	
+//	@Override
+//	public void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		
+//		new Thread(new ClientThread()).start();
+//	}
+//
+//	public void onClick(View view) {
+//		try {
+////			EditText et = (EditText) findViewById(R.id.editText_sendClient);
+////			String str = et.getText().toString();
+////			PrintWriter out = new PrintWriter(new BufferedWriter(
+////					new OutputStreamWriter(socket.getOutputStream())),
+////					true);
+////			out.println(str);
+//		} catch (UnknownHostException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	class ClientThread implements Runnable {
+//
+//		@Override
+//		public void run() {
+//		//TODO EMÝN DEÐÝLÝM
+//			
+//			try {
+//				InetAddress serverAddr = null;
+//				while(!serverAddr.isReachable(10000)){ // bu kýsýmdan tam emin deðilim
+//				for(int i=1;i<=255;i++){ 
+//					
+//				SERVER_IP2=i;
+//				SERVER_IP=SERVER_IP1+String.valueOf(SERVER_IP2);
+//				InetAddress.getByName(SERVER_IP);
+//					
+//				if(serverAddr.isReachable(10000)){
+//					
+//					Toast.makeText(getApplicationContext(),
+//							"Connected to "+serverAddr,
+//							Toast.LENGTH_SHORT);
+//					
+//					
+//				}
+//				else{
+//					Toast.makeText(getApplicationContext(),
+//							"Failed to connect "+serverAddr,
+//							Toast.LENGTH_LONG);
+//				}
+//					
+//				}
+//				}
+//			
+//				socket = new Socket(serverAddr, SERVERPORT);
+//
+//			} catch (UnknownHostException e1) {
+//				e1.printStackTrace();
+//			} catch (IOException e1) {
+//				e1.printStackTrace();
+//			}
+//
+//		}
+//
+//	}
+//}
